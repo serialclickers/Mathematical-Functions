ef41a42fe065db8d82a57e4c98fd66c92e38639d
Serial Clickers <admin@serialclickers.com>
2012-02-29 21:01:06 +0200

    Update pmt.js

--- pmt.js
+++ pmt.js
@@ -4,6 +4,7 @@ function PMT (ir, np, pv, fv, mtype ) {
  np - number of periods (months)
  pv - present value
  fv - future value (residual value)
+ mtype - 0/1
  */
 
  pmt = ( ir * ( pv * Math.pow ( (ir+1), np ) + fv ) ) / ( ( ir * mtype + 1 ) * ( Math.pow ( (ir+1), np) -1 ) );
